2804a9d1dfe7acb1eb17c3e7c54f4d786a37f666
shristy <shristychaudhary4679@gmail.com>
2025-05-24 18:18:44 +0530

    add animations in HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from "react"
+import {motion} from 'framer-motion'
 import { Anton } from "next/font/google"
 import { Unbounded } from 'next/font/google'
 const anton = Anton({ subsets: ['latin'], weight: '400', display: 'swap' })
@@ -20,7 +21,16 @@ useEffect(() => {
   return () => window.removeEventListener('scroll', handleScroll);
 }, []);
 
-
+const lines = [
+  "       I'm a full-stack developer ",
+  "based in Bengaluru and a second",
+  "year Information Science and",
+  "Engineering student at NMIT. I",
+  "enjoy reading, drawing, and diving ",
+  "into history in my free time."
+];
+const duration = 1.5;
+const gap = 0.01;
     return (
       <section className="relative min-h-[150vh] bg-cover bg-top" style={{ backgroundImage: "url('/hero-image.png')" }}>
         <div
@@ -30,11 +40,35 @@ useEffect(() => {
         <div className="absolute inset-0 flex flex-col text-white transition duration-300 py-40 px-6 md:py-64 md:px-10">
           <div className="flex flex-col md:flex-row items-start md:items-center justify-between space-y-10 md:space-y-0 md:space-x-10">
             <div className={`${anton.className} text-5xl sm:text-7xl md:text-9xl leading-tight`}>
-              <div>SHRISTY</div>
-              <div>CHAUDHARY</div>
+              <div><motion.div
+                initial={{y: '100%', opacity: 0}}
+                animate={{y: 0, opacity: 1}}
+                transition={{duration: 0.8}}
+              >
+                    SHRISTY
+                </motion.div></div>
+              <div><motion.div
+                initial={{y: '100%', opacity: 0}}
+                animate={{y: 0, opacity: 1}}
+                transition={{duration: 0.8, delay: 0.3}}
+              >
+                  CHAUDHARY
+                </motion.div></div>
             </div>
             <div className={`${unbounded.className} max-w-md text-lg sm:text-xl md:text-3xl`}>
-              I&apos;m a full-stack developer based in Bengaluru and a second-year Information Science and Engineering student at NMIT. I enjoy reading, drawing, and diving into history in my free time.
+              <div className="-ml-40">
+                {lines.map((line, i) => (
+                  <motion.div
+                    key={i}
+                    style={{ overflow: 'hidden', whiteSpace: 'pre' }}
+                    initial={{ width: 0 }}
+                    animate={{ width: '100%' }}
+                    transition={{ duration, ease: 'easeInOut', delay: i * (duration + gap) }}
+                  >
+                    {line}
+                  </motion.div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
